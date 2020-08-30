e6081b0593bac968475f948bc803a154f80d5e72
rk011616@ohio.edu <kntusenrowan@gmail.com>
2020-08-29 23:56:43 -0400

    die-main.cc created

---
+++ die-main.cc
@@ -0,0 +1,25 @@
+/**
+ * Author: Rowan Knutsen
+ * Date: 28 Aug 2020
+ * Description: Program simulating the rolling of a 6 sided die
+ */
+
+#include <iostream>
+#include <iomainip>
+#include <cstdlib>
+#include "die.h"
+using namespace std;
+//function prototype
+
+int main(){
+
+    Die d;
+    cout << "Face Value: " << d << endl;
+    Die d12(12);
+    cout << "Face Value: " << d12 << endl;
+    d12.setNumSides(24);
+    for(size_t i = 0; i < 6; i++){
+        cout << "Roll: " << d12.roll() << endl;
+    }
+    return 0;
+}
